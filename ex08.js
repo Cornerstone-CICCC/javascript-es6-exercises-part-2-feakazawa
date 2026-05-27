e5d2c7a59678ca78c6f956fc704fc54597068da8
Fernanda Akazawa <fernandakazawa@gmail.com>
2026-05-27 12:35:33 -0700

    exercise 8 done

--- ex08.js
+++ ex08.js
@@ -1,3 +1,6 @@
 // Join Exercise
-// Instructions: Create an array of at least five different words as strings. Write code to join all the words into a single string, with each word separated by a space.
+// Instructions: Create an array of at least five different words as strings. Write code to join all the words into a single string,
+// with each word separated by a space.
 
+const words = ["hello", "ola", "Fer", "Vancouver", "dollarama", "costco"];
+console.log(words.join(" "));
