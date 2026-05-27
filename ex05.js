75e9bdfc0d4654e3953f58bf0a10580815a60af2
Fernanda Akazawa <fernandakazawa@gmail.com>
2026-05-27 12:25:20 -0700

    exercise 5 done

--- ex05.js
+++ ex05.js
@@ -1,3 +1,5 @@
 // Some Exercise
 // Instructions: Create an array of at least five numbers. Write code to check if some of the numbers are even.
 
+const numbers = [1, 3, 4, 7, 9, 12, 14, 11, 0, 4];
+console.log(numbers.some((num) => num % 2 === 0));
